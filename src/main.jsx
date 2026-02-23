f53d8fe39732fb01d12482590d0404b06b678afe
yahyatatari93-code <yahya.tatari93@gmail.com>
2026-02-23 10:58:37 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// كود تشغيل التطبيق كـ PWA (تطبيق موبايل)
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(() => {
+      console.log('Service Worker Registered');
+    });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
